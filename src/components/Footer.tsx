ff8c8f976dbc970fede276617d1e4863b2a41afc
Thomas Erhel <thomas.erhel@gmail.com>
2021-04-17 03:04:33 +0200

    Update footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
                 <p>**This is a portfolio. All the projects and contact details given are real.</p>
                 <p>
                     &copy; Created for
-    <a href="https://www.monproweb.io/" target="_blank" rel="noreferrer"
+    <a href="https://monproweb.io/" target="_blank" rel="noreferrer"
                     > Mon Pro Web <i className="fab fa-react"></i
                     ></a>
                 </p>
